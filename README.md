738617cac0960c6a0f75139b78ba28bba1ac362d
ceddlyburge <ceddlyburge@gmail.com>
2020-11-14 16:16:53 +0000

    Update readme
    And remove notes from tests

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# mars-rover-kata
+# Mars Rover Kata
+
+## Instructions
+
+2- 3 hours
+kiss
+use the sample data
+do the hard things first
+commit frequently
+
+## Taking it further
+
+It might be better to have separate types for the inputs and for the domain. This would allow Location to have a Mars property, which makes logical sense, and would probably mean less parameters to pass around. It would also stop an invalid combination of Mars and Location being used together.
+
+The Scent concept is only weakly represented in the code, and could be strengthened, probably with a Scent type.
+
+`mapAccuml` is a relatively complicated thing, and relies on a Tuple, as opposed to a domain type. It might be better to pattern match on the list instead and create a domain type.
+
+The names of the things in code should be the same as the domain (the instructions). I have tried to do this, but a check would probably reveal some things.
+
+There is only one implementation module and one test module, but ideally these would be split up in to smaller, more cohesive modules.
+
+A bit of a tidy up, especially in the tests, would probably be good. Some of the tests are quite long and have a lot of confusing brackets.
+
+There is a minor primitive obsession on Int, in the Location and Mars types. This is hard to remove, but if these were in their own modules, opaque types could be used to make sure the obession didn't leak out of the module. It would also allow easy addition of other world types (such as non rectangular worlds). A world with a different number of dimensions would be difficult to encapsulate though.
--- tests/RoverTests.elm
+++ tests/RoverTests.elm
@@ -9,37 +9,6 @@ import Fuzz exposing (Fuzzer, list, string)
 import Rover exposing (..)
 
 
--- ## instructions
-
--- 2- 3 hours
--- kiss
--- use the sample data
--- do the hard things first
--- commit frequently
-
--- need to keep last known positions for lost robots (for the output)
--- lost robots leave a scent at their last valid position (maybe the above could meet this demand)
--- north is y + 1
--- east is x + 1
--- The first robot finish all its movement before the second robot starts and so on
-
--- ## tests
--- slimed: sample data for test (input to output)
--- done (could test / fuzz more cases): update robot position existing position and instruction (ignore scents)
--- done (could test / fuzz more cases): update robot position existing position and instruction (ignore scents), becoming lost (must update scents)
--- done: update robot position and scent from lost position, becoming lost, make sure last known position is retained
--- done: update robot position and scent from existing position, existing scents and instruction
--- done: update robot position and scent from existing position, existing scents and instruction, becoming lost, make sure last known position is recorded
--- done: Apply list of instructions to a robot, from an initial position
--- done: Apply list of instructions to list of robots, from a list of initial positions
--- done, although not well tested: calculate scents from last known locations of lost robots
--- done: parse mars
--- done: parse initial position of robot in to type
--- done: parse robot instructions in to type
--- done: parse all inputs
--- output of known position of robot (for final output)
--- output of lost position of robot, including last know position (for final output)
-
 tests : Test
 tests =
     describe "rove"
